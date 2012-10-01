e797c2fae67c989a56565e8be1c5879e6c51e145
Christine McClure <camcclure@gmail.com>
2012-10-01 18:54:12 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Cocktail.delete_all
+Cocktail.create(:name => "Sazerac", :notes => "Mix all")
+Cocktail.create(:name => "Singapore Sling", :notes => "Mix all")
+Cocktail.create(:name => "Manhattan", :notes => "Mix all")
+
+Ingredient.delete_all
+Ingredient.create(:ingredient => "3 oz rye whiskey", :cocktail_id => 1)
+Ingredient.create(:ingredient => "3/4 oz simple syrup", :cocktail_id => 1)
+Ingredient.create(:ingredient => "spash absinthe", :cocktail_id => 1)
+
+Ingredient.create(:ingredient => "1/2 oz grenadine", :cocktail_id => 2)
+Ingredient.create(:ingredient => "1 oz gin", :cocktail_id => 2)
+Ingredient.create(:ingredient => "club soda", :cocktail_id => 2)
